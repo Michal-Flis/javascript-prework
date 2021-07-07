c9a45b12b3c42ba448d163ff6ff4faf006f473c2
Michał Flis <michal.flis@op.pl>
2021-07-07 14:47:50 +0200

    Player Move

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
-
+//Ruch komputera
 if(randomNumber == 1){
   computerMove = 'kamień';
 }else if(randomNumber == 2){
@@ -9,3 +9,22 @@ if(randomNumber == 1){
 }else if(randomNumber == 3){
     computerMove = 'nożyce'
 }
+
+printMessage('Ruch komputera to:' + computerMove)
+
+//Ruch Gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if(randomNumber == 2){
+    playerMove = 'papier'
+}else if(randomMove == 3){
+    playerMove = 'nożyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
