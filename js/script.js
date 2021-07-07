ac232221384de358d4cb4a0ce96e27c27cdad369
Michał Flis <michal.flis@op.pl>
2021-07-07 14:40:16 +0200

    Computer Move

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-let computerMove = 'kamień'
-let playerMove = 'papier'
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = 'nieznany ruch';
 
-
-printMessage("'Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'");
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}else if(randomNumber == 2){
+    computerMove = 'papier'
+}else if(randomNumber == 3){
+    computerMove = 'nożyce'
+}
